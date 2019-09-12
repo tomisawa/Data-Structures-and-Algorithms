268b9f32821f5d7df3e587dc592f7e180fec2685
tomisawa <tomisawa@maebashi-it.ac.jp>
2019-09-12 22:51:58 +0900

    keisokuのヘッダファイル

---
+++ keisoku.h
@@ -0,0 +1,18 @@
+//
+//  keisoku.h
+//  DAS01
+//
+//  Created by 冨澤眞樹 on 2019/09/10.
+//  Copyright © 2019 Masaki Tomisawa. All rights reserved.
+//
+
+#ifndef keisoku_h
+#define keisoku_h
+
+int PrintClockRes(void);
+int StartClock(void);
+int StopClock(void);
+int PrintClockSpan(void);
+double CalcClockSpan(void);
+
+#endif /* keisoku_h */
